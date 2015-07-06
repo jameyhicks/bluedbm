e21418f9a8e3921ddd105092a9332b7478a2ebe4
Jamey Hicks <jamey.hicks@gmail.com>
2015-07-06 13:30:00 -0400

    revise synth-aurora-ext.tcl so that each core can be generated independently

--- core-scripts/synth-aurora-ext.tcl
+++ core-scripts/synth-aurora-ext.tcl
@@ -1,23 +1,22 @@
 source board.tcl
 source $connectaldir/scripts/connectal-synth-ip.tcl
 
-set core_version "9.2"
-if {[version -short] >= "2014.4"} {
-    set core_version "9.3"
+set locs {24 25 26 27 16 17 18 19}
+if [info exists env(AURORA_LOCS)] {
+    set locs $env(AURORA_LOCS)
 }
 
-connectal_synth_ip aurora_64b66b $core_version aurora_64b66b_X1Y24 [list CONFIG.C_LINE_RATE {10.0} CONFIG.C_REFCLK_FREQUENCY {625.000} CONFIG.interface_mode {Streaming} CONFIG.C_GT_LOC_25 {1} CONFIG.C_GT_LOC_1 {X}]
+set core_version 9.2
 
-connectal_synth_ip aurora_64b66b $core_version aurora_64b66b_X1Y25 [list CONFIG.C_LINE_RATE {10.0} CONFIG.C_REFCLK_FREQUENCY {625.000} CONFIG.interface_mode {Streaming} CONFIG.C_GT_LOC_26 {1} CONFIG.C_GT_LOC_1 {X}]
+if {[version -short] >= "2014.4"} {
+    set core_version {9.3}
+}
+if {[version -short] >= "2015.2"} {
+    ## this version changed pinout
+    set core_version {10.0}
+}
 
-connectal_synth_ip aurora_64b66b $core_version aurora_64b66b_X1Y26 [list CONFIG.C_LINE_RATE {10.0} CONFIG.C_REFCLK_FREQUENCY {625.000} CONFIG.interface_mode {Streaming} CONFIG.C_GT_LOC_27 {1} CONFIG.C_GT_LOC_1 {X}]
-
-connectal_synth_ip aurora_64b66b $core_version aurora_64b66b_X1Y27 [list CONFIG.C_LINE_RATE {10.0} CONFIG.C_REFCLK_FREQUENCY {625.000} CONFIG.interface_mode {Streaming} CONFIG.C_GT_LOC_28 {1} CONFIG.C_GT_LOC_1 {X}]
-
-connectal_synth_ip aurora_64b66b $core_version aurora_64b66b_X1Y16 [list CONFIG.C_LINE_RATE {10.0} CONFIG.C_REFCLK_FREQUENCY {625.000} CONFIG.interface_mode {Streaming} CONFIG.C_GT_LOC_17 {1} CONFIG.C_GT_LOC_1 {X}]
-
-connectal_synth_ip aurora_64b66b $core_version aurora_64b66b_X1Y17 [list CONFIG.C_LINE_RATE {10.0} CONFIG.C_REFCLK_FREQUENCY {625.000} CONFIG.interface_mode {Streaming} CONFIG.C_GT_LOC_18 {1} CONFIG.C_GT_LOC_1 {X}]
-
-connectal_synth_ip aurora_64b66b $core_version aurora_64b66b_X1Y18 [list CONFIG.C_LINE_RATE {10.0} CONFIG.C_REFCLK_FREQUENCY {625.000} CONFIG.interface_mode {Streaming} CONFIG.C_GT_LOC_19 {1} CONFIG.C_GT_LOC_1 {X}]
-
-connectal_synth_ip aurora_64b66b $core_version aurora_64b66b_X1Y19 [list CONFIG.C_LINE_RATE {10.0} CONFIG.C_REFCLK_FREQUENCY {625.000} CONFIG.interface_mode {Streaming} CONFIG.C_GT_LOC_20 {1} CONFIG.C_GT_LOC_1 {X}]
+foreach loc $locs {
+    set loc_plus_1 [expr $loc + 1]
+    connectal_synth_ip aurora_64b66b $core_version aurora_64b66b_X1Y$loc [list CONFIG.C_LINE_RATE {10.0} CONFIG.C_REFCLK_FREQUENCY {625.000} CONFIG.interface_mode {Streaming} CONFIG.C_GT_LOC_$loc_plus_1 {1} CONFIG.C_GT_LOC_1 {X}]
+}
